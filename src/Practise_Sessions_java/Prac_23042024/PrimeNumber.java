0e0313e2a8977467db5341bad7f1b3c001c65d4d
Akhilpalthi <akhilpalthi@gmail.com>
2024-04-29 22:52:31 +0530

    Practise class of 25th April

--- src/Practise_Sessions_java/Prac_23042024/PrimeNumber.java
+++ src/Practise_Sessions_java/Prac_23042024/PrimeNumber.java
@@ -4,25 +4,23 @@ public class PrimeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Number");
-        long num = sc.nextLong();
-        boolean isPrime=false;
+        int num = sc.nextInt();
+        boolean isPrime=true;
         if (num <= 1) {
-            System.out.println("Please Enter a Natural Positive integer");
+            System.out.println("Please Enter a Natural Positive integer Greater than 1");
         } else {
             for (int i = 2; i <= Math.sqrt(num); i++) {
                 if (num % i == 0) {
-                    isPrime =true;
-                } else {
-                    isPrime=false;
+                    isPrime =false;
+                    break;
                 }
-
-
             }
-        }
-        if(isPrime){
-            System.out.println("Number is Prime");
-        }else{
-            System.out.println("Number is not prime");
+            if(isPrime){
+                System.out.println("Number is Prime");
+            }else{
+                System.out.println("Number is not prime");
+            }
+
         }
         sc.close();
     }
